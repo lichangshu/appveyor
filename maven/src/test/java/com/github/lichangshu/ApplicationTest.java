1db26c1837dc402d427d91eb79c45be523970dcf
changshu.li <lcs.005@163.com>
2021-11-12 18:01:48 +0800

    maven hello

---
+++ maven/src/test/java/com/github/lichangshu/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.github.lichangshu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    public void testMain() {
+        Application.main(new String[]{});
+        assertTrue(true);
+    }
+
+}
